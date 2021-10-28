fb2f3a207cd440853ef4db1ced255e92f3c09c60
matthewchoiyiuhei <matthew.choi@dotdotfire.com>
2021-10-29 00:17:17 +0800

    First push

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactLive2d from 'react-live2d';
+
+const App = () => (
+  <>
+    <ReactLive2d
+        width={300}
+        height={500}
+    />
+  </>
+);
+export default App;
